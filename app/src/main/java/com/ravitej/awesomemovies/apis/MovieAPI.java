985cf7d0ca752e4f470aac8d2686b94c72993688
Ravi Tej Kallepalli <ravitej.kallepalli@gmail.com>
2020-06-18 18:24:38 -0400

    Removed the API_KEY

--- app/src/main/java/com/ravitej/awesomemovies/apis/MovieAPI.java
+++ app/src/main/java/com/ravitej/awesomemovies/apis/MovieAPI.java
@@ -1,14 +1,15 @@
 package com.ravitej.awesomemovies.apis;
 
 import com.ravitej.awesomemovies.dtos.MovieDTO;
+import com.ravitej.awesomemovies.network.NetworkUtils;
 import io.reactivex.Single;
 import retrofit2.http.GET;
 
 public interface MovieAPI {
 
-    @GET("movie/popular?api_key=3ddb3bad87271ff98bac18734a2591f6")
+    @GET("movie/popular?api_key=" + NetworkUtils.API_KEY)
     Single<MovieDTO> fetchPopularMovies();
 
-    @GET("movie/top_rated?api_key=3ddb3bad87271ff98bac18734a2591f6")
+    @GET("movie/top_rated?api_key=" + NetworkUtils.API_KEY)
     Single<MovieDTO> fetchTopRatedMovies();
 }
--- app/src/main/java/com/ravitej/awesomemovies/network/NetworkUtils.java
+++ app/src/main/java/com/ravitej/awesomemovies/network/NetworkUtils.java
@@ -2,6 +2,7 @@ package com.ravitej.awesomemovies.network;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.ravitej.awesomemovies.BuildConfig;
 import com.ravitej.awesomemovies.apis.MovieAPI;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -14,6 +15,7 @@ public class NetworkUtils {
 
     public static final String BASE_MOVIE_URL = "https://api.themoviedb.org/3/";
     public static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w342/";
+    public static final String API_KEY = BuildConfig.API_KEY;
 
     private Retrofit retrofit;
     private static NetworkUtils instance;
